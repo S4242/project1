4e805322da00cf342dd34026bb5e1bc4b229917c
S4 <rydvv.ua@gmail.com>
2019-08-09 13:57:42 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,13 @@
+"use strict";
+var leftBordetWidgh = 1;
+
+let second = 2;
+const pi = 3.14;
+
+//console.log(pi);
+
+//alert ("Hello World!");
+//let answer = confirm ("Are you here?");
+//console.log (answer);
+let answer = prompt ("Есть ли Вам 18?", "Да");
+console.log (answer);
